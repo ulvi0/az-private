fd9798d05a950cffeb439db01dc27fc660b2c92f
ulvi0 <techn9neebah>
2019-10-17 08:39:07 +0400

    -- random number generator -- pom.xml -- gitignore

---
+++ src/main/java/RandomNmbers.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class RandomNmbers {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int [] a = new int[10];
+        System.out.println("Random Numbers: ");
+        for(int i = 0; i< 10; i++){
+            a[i] = random.nextInt(11) + 10;
+            System.out.print(a[i]);
+            System.out.print(" ");
+        }
+
+    }
+}
